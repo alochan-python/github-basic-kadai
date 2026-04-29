214669c8883ab5b9db25ebed1a29a45e8607a04e
alochan-python <kazu_naka_kazumasa17@yahoo.co.jp>
2026-04-29 20:18:15 +0900

    Add files via upload

--- JavaScript/kadai_mdn_js/mdn.js
+++ JavaScript/kadai_mdn_js/mdn.js
@@ -1,7 +1,6 @@
 const nowday = new Date();
-const year = nowday.getFullYear()
-const month = nowday.getMonth()
+const year = nowday.getFullYear();
+const month = nowday.getMonth() +1;
 const date = nowday.getDate();
 
-console.log(nowday);
 console.log(`${year}年${month}月${date}日`);
